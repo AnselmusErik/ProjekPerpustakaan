9898957a8a5c1238edcbc7e68e6ba083d11375f2
AnselmusErik <anselmuserik11@gmail.com>
2024-05-28 21:27:54 +0700

    sementara

--- HomeMhs/home.php
+++ HomeMhs/home.php
@@ -5,7 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Home Page</title>
+    <style>
+        .card {
+            height: 100%;
+            position: relative;
+        }
+
+        .btn-pinjam {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+        }
+        .card-img-top {
+            height: auto;
+            max-height: 200px;
+            object-fit: contain;
+        }
+        .card-body {
+            padding-bottom: 40px;
+        }
+    </style>
+    <title>Library</title>
 </head>
 
 <body>
@@ -24,38 +44,40 @@
     <!-- Content -->
     <div class="container mt-5">
         <div class="row">
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="/img/artikel_1_standby.png" class="card-img-top img-fluid" alt="Gambar Artikel">
-                    <div class="card-body">
-                        <h5 class="card-title">Cari dan Temukan Resep dari Kuresep Masak</h5>
-                        <a href="#" class="btn btn-primary">Pinjam</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="/img/artikel_1_standby.png" class="card-img-top img-fluid" alt="Gambar Artikel">
-                    <div class="card-body">
-                        <h5 class="card-title">Cari dan Temukan Resep dari Kuresep Masak</h5>
-                        <a href="#" class="btn btn-primary">Pinjam</a></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <img src="/img/artikel_1_standby.png" class="card-img-top img-fluid" alt="Gambar Artikel">
-                    <div class="card-body">
-                        <h5 class="card-title">Cari dan Temukan Resep dari Kuresep Masak</h5>
-                        <a href="#" class="btn btn-primary">Pinjam</a>
-                    </div>
-                </div>
-            </div>
+            <?php
+            // Koneksi ke database
+            $link = mysqli_connect("localhost", "root", "", "librarydb");
 
+            // Query untuk mengambil data buku
+            $sql = "SELECT code_buku, judul_buku, gambar, nama_pengarang, penerbit, tahun_terbit FROM buku ORDER BY RAND() LIMIT 3";
+            $result = mysqli_query($link, $sql);
+
+            // Tampilkan data buku
+            while ($row = mysqli_fetch_assoc($result)) {
+                $gambarBase64 = base64_encode($row['gambar']);
+                echo '
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="data:image/png;base64,' . $gambarBase64 . '" class="card-img-top img-fluid" alt="Gambar Buku">
+                        <div class="card-body">
+                            <h5 class="card-title">' . $row['judul_buku'] . '</h5>
+                            <p>ISBN: ' . $row['code_buku'] . '</p>
+                            <p>Pengarang: ' . $row['nama_pengarang'] . '</p>
+                            <p>Penerbit: ' . $row['penerbit'] . '</p>
+                            <p>Tahun Terbit: ' . $row['tahun_terbit'] . '</p>
+                            <a href="#" class="btn btn-primary">Pinjam</a>
+                        </div>
+                    </div>
+                </div>';
+            }
+
+            // Tutup koneksi
+            mysqli_close($link);
+            ?>
         </div>
+    </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
